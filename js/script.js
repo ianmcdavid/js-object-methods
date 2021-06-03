5bbec7bbd1978c6d4993bffd3e5ef46c07e6e1f5
EMcD <64993802+ianmcdavid@users.noreply.github.com>
2021-06-02 23:21:05 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const topicElement = document.querySelector(".topic");
+const countElement = document.querySelector(".count");
+const body = document.querySelector("body");
+const success = document.querySelector(".success");
+const dance = document.querySelector(".dance");
+
+const learning = {
+  topic: "JS",
+  learningGoals: [
+    "Understand programming fundamentals",
+    "Have Fun",
+    "Build JS programs"
+  ],
+  category: "Front End Development",
+  topicImportance: "high",
+  hoursThisWeek: 2,
+  weeklyHourGoal: 7,
+  achievedStudyGoals: "false",
+  addStudyTime: function (hours) {
+    this.hoursThisWeek += hours;
+    if (this.hoursThisWeek >= this.weeklyHourGoal) {
+      this.achievedStudyGoals = "true";
+      this.celebrate();
+    }
+  },
+  celebrate: function () {
+    body.classList.add("celebrate");
+    success.classList.remove("hide");
+    dance.classList.remove("hide");
+  }
+};
+
+topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
+countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals.`;
+
+topicElement.classList.remove("hide");
+countElement.classList.remove("hide");
+
+learning.addStudyTime(10);
